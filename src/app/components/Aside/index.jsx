97ac4cf484a5f7820a29df9e4db276e2ba70d2ad
luis.henrique <luis.henrique@fpf.br>
2025-10-17 14:39:31 -0400

    WIP screen

--- src/app/components/Aside/index.jsx
+++ src/app/components/Aside/index.jsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import styles from './aside.module.css'
+import logo from './logo.png'
 
 export const Aside = () => {
     return (<aside className={styles.aside}>
-        <img className={styles.img} src="/logo.png" alt="logo CALU"/>
+        <Image src={logo} className={styles.img}/>
+        {/* <img className={styles.img} src="/logo.png" alt="logo CALU"/> */}
         </aside>)
 
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,26 @@
 import { Aside } from './components/Aside';
 import './globals.css'
+import {Prompt, Raleway} from 'next/font/google'
 
 export const metadata = {
   title: "CALU",
   description: "Cafés especiais!",
 };
 
+const prompt = Prompt({
+  weight:['400','600'],
+  subsets:['latin'],
+  display:'swap'
+}) 
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={prompt.className}>
       <body>
-        <Aside/>
-        {children}
+        <div className='app-container'>
+          <Aside/>
+          {children}
+        </div>
       </body>
     </html>
   );
